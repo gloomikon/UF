3eb4cb78a63586944b76599a9dea17ac25eb8d85
gloomikon <gloomikon@gmail.com>
2019-10-31 18:32:30 +0200

    work

--- Swifty Companion/Swifty Companion/Controller/BioController.swift
+++ Swifty Companion/Swifty Companion/Controller/BioController.swift
@@ -1,12 +1,32 @@
 import UIKit
 
 class BioController: UIViewController {
+    @IBOutlet weak var levelProgress: UIProgressView!
+    @IBOutlet weak var levelText: UILabel!
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var displayname: UILabel!
+    @IBOutlet weak var login: UILabel!
     
+    let layer = CAGradientLayer()
     override func viewDidLoad() {
         super.viewDidLoad()
-        photo.layer.cornerRadius = 50
+        photo.layer.cornerRadius = 75
+
+        //levelProgress.transform = levelProgress.transform.scaledBy(x: 1, y: 50)
+        levelText.text = String(format: "%.2f", serverData.user!.level)
+        levelProgress.progress = Float(serverData.user!.level - floor(serverData.user!.level))
+        levelProgress.layer.cornerRadius = 10
+        levelProgress.clipsToBounds = true
+
+        displayname.text = serverData.user?.displayname
+        login.text = serverData.user?.login
+
+        layer.frame = view.bounds
+        layer.colors = [UIColor.cyan.cgColor, UIColor(red: 33/255, green: 33/255, blue: 117/255, alpha: 1).cgColor, UIColor.purple.cgColor]
+        view.layer.insertSublayer(layer, at: 0)
+
+
         if UIDevice.current.orientation.isLandscape {
             stackView.axis = .horizontal
         } else {
@@ -29,5 +49,10 @@ class BioController: UIViewController {
         } else {
             stackView.axis = .vertical
         }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        layer.frame = view.bounds
     }
 }
--- Swifty Companion/Swifty Companion/Controller/SearchController.swift
+++ Swifty Companion/Swifty Companion/Controller/SearchController.swift
@@ -5,12 +5,16 @@ class SearchController: UIViewController {
     @IBOutlet weak var searchField: UITextField!
     
     var apiSearchController: APISearchController?
+    let layer = CAGradientLayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         apiSearchController = APISearchController(delegate: self)
         self.navigationItem.hidesBackButton = true
         searchBtn.layer.cornerRadius = 10
+        layer.frame = view.bounds
+        layer.colors = [UIColor.yellow.cgColor, UIColor.purple.cgColor, UIColor.black.cgColor]
+        view.layer.insertSublayer(layer, at: 0)
     }
     
     @IBAction func search(_ sender: UIButton) {
@@ -23,5 +27,10 @@ class SearchController: UIViewController {
     
     public func gotoBio() {
         self.performSegue(withIdentifier: "gotoBio", sender: nil)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        layer.frame = view.bounds
     }
 }
